99cc1cda700c73e3d513ad44be927ad6c20e3660
dolaned <dylan.aird595@gmail.com>
2018-10-11 07:40:05 -0700

    added photon specific stuff and build files

---
+++ build/CMakeFiles/secp256k1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secp256k1"
+  "CMakeFiles/secp256k1-complete"
+  "deps/secp256k1/src/secp256k1-stamp/secp256k1-install"
+  "deps/secp256k1/src/secp256k1-stamp/secp256k1-mkdir"
+  "deps/secp256k1/src/secp256k1-stamp/secp256k1-download"
+  "deps/secp256k1/src/secp256k1-stamp/secp256k1-update"
+  "deps/secp256k1/src/secp256k1-stamp/secp256k1-patch"
+  "deps/secp256k1/src/secp256k1-stamp/secp256k1-configure"
+  "deps/secp256k1/src/secp256k1-stamp/secp256k1-build"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/secp256k1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/spvtest.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spvtest.dir/BRAddress.c.o"
+  "CMakeFiles/spvtest.dir/BRBIP32Sequence.c.o"
+  "CMakeFiles/spvtest.dir/BRBIP38Key.c.o"
+  "CMakeFiles/spvtest.dir/BRBIP39Mnemonic.c.o"
+  "CMakeFiles/spvtest.dir/BRBase58.c.o"
+  "CMakeFiles/spvtest.dir/BRBloomFilter.c.o"
+  "CMakeFiles/spvtest.dir/BRCrypto.c.o"
+  "CMakeFiles/spvtest.dir/BRKey.c.o"
+  "CMakeFiles/spvtest.dir/BRMerkleBlock.c.o"
+  "CMakeFiles/spvtest.dir/BRPaymentProtocol.c.o"
+  "CMakeFiles/spvtest.dir/BRPeer.c.o"
+  "CMakeFiles/spvtest.dir/BRPeerManager.c.o"
+  "CMakeFiles/spvtest.dir/BRSet.c.o"
+  "CMakeFiles/spvtest.dir/BRTransaction.c.o"
+  "CMakeFiles/spvtest.dir/BRWallet.c.o"
+  "CMakeFiles/spvtest.dir/test.c.o"
+  "spvtest.pdb"
+  "spvtest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/spvtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
